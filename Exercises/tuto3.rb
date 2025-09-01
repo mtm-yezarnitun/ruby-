5e8b70a948493b94a48dc38a38d409d2ebd7b0a3
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-01 10:39:25 +0630

    - ruby exercises -

--- Exercises/tuto3.rb
+++ Exercises/tuto3.rb
@@ -1,36 +1,61 @@
-n=1
-  while n == 1
-    print "Enter First Number : "
-    num1 = gets.chomp.to_f
-    
-    print "Chose an Operator : (+ , - , * , / ) "
-    op = gets.chomp.to_s
-    
-    print "Enter Second Number : "
-    num2 = gets.chomp.to_f
-    
-    case op 
-    when "+"
-      result = num1 + num2
-      puts "Result = #{result}"
+n = 1
 
-    when "-"
-      result = num1 - num2
-      puts "Result = #{result}"
-    when "*"
-      result = num1 * num2
-      puts "Result = #{result}"
-    when "/"
+while n == 1
+  num1=nil
+  loop do
+    begin
+      print "Enter First Number: "
+      num1 = Float(gets.chomp)
+      break
+    rescue ArgumentError
+      puts "Invalid Input! Enter Numbers Only."
+    end
+  end
+  
+  num2=nil
+   loop do
+    begin
+      print "Enter Second Number: "
+      num2 = Float(gets.chomp)
+      break
+    rescue ArgumentError
+      puts "Invalid Input! Enter Numbers Only."
+    end
+  end
+
+  print "Choose an Operator (+, -, *, /): "
+  op = gets.chomp.to_s
+
+  case op
+  when "+"
+    result = num1 + num2
+    puts "Result = #{result}"
+  when "-"
+    result = num1 - num2
+    puts "Result = #{result}"
+  when "*"
+    result = num1 * num2
+    puts "Result = #{result}"
+  when "/"
+    if num2 == 0.0
+      puts "Error: Cannot divide by 0."
+    else
       result = num1 / num2
       puts "Result = #{result}"
     end
-    
-    print "Do you wanna continue ? ( y/ n ) "
-    con = gets.chomp.to_s
-      case con 
-      when "y"
-        n = 1
-      when "n"
-        n = 0
-      end
+  else
+    puts "Invalid Operator!"
   end
+
+  print "Do you wanna continue? (y/n): "
+  con = gets.chomp.downcase
+  case con
+  when "y"
+    n = 1
+  when "n"
+    n = 0
+  else
+    puts "Invalid Option! Exiting..."
+    n = 0
+  end
+end
--- Exercises/tuto7.rb
+++ Exercises/tuto7.rb
@@ -1,17 +1,23 @@
 class Person
+  attr_accessor :name
+
   def initialize(name="")
     @name =name
   end
+
   def print_name 
     puts "Your Name is : #{@name}"
   end
 end
 
 class Student < Person
-  def initialize(name="",roll="")
-    @name = name
+  attr_accessor :roll
+
+  def initialize(name="", roll="")
+    super(name)
     @roll = roll
   end
+
   def print_roll 
     puts "Roll Number is : #{@roll}"
   end
@@ -25,7 +31,7 @@ when "person"
   print "Enter Your Name : "
   pname = gets.chomp.to_s
 
-  newperson = Person.new(pname)
+  newperson= Person.new(pname)
 
   newperson.print_name
 
@@ -33,12 +39,21 @@ when "student"
   print "Enter Your Name : "
   sname=gets.chomp.to_s
 
-  print "Enter Your Roll Number : "
-  sroll=gets.chomp.to_s
+  sroll=nil
+   loop do
+    begin
+      print "Enter Your Roll Number : "
+      sroll = Integer(gets.chomp)
+      break
+    rescue ArgumentError
+      puts "Invalid Input! Enter Numbers Only."
+    end
+  end
 
   newstudent= Student.new(sname,sroll)
 
   newstudent.print_name
   newstudent.print_roll
-
+else 
+  print "Invalid Input! Exiting....."
 end
